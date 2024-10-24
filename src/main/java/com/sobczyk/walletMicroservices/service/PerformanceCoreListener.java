87c000e6820afc0af97e2d600848e13a7ce5c627
MateuszSobczyk-ti <mateusz112299@gmail.com>
2024-10-24 19:57:08 +0200

    Send response to rabbit report queue

--- src/main/java/com/sobczyk/walletMicroservices/service/PerformanceCoreListener.java
+++ src/main/java/com/sobczyk/walletMicroservices/service/PerformanceCoreListener.java
@@ -1,13 +1,12 @@
 package com.sobczyk.walletMicroservices.service;
 
 import com.sobczyk.walletMicroservices.dto.requests.PositionPerformanceRequest;
+import com.sobczyk.walletMicroservices.dto.responses.PositionPerformanceResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Slf4j
 @Service
@@ -18,19 +17,8 @@ public class PerformanceCoreListener {
     private final RabbitTemplate rabbitTemplate;
 
     @RabbitListener(queues = {"q.performance-core"})
-    public void generatePerfReport(List<PositionPerformanceRequest> requestList) {
-        log.info("received new message from rabbit queue: " + requestList);
-        System.out.println(requestList.size());
-        throw new RuntimeException("constraint violation");
-//        rabbitTemplate.convertAndSend("x.performance-report", "performance-report", requestList);
-//        log.info("New message added on queue");
- //       return ResponseEntity.ok("request added to queue");
-//        requestList.stream()
-//                .map(r -> positionPerfService.getPositionPerformance(r.investorId(), r.timeSeries()))
-//                .forEach(r -> {
-//                    System.out.println(r.getTransactions().toString());
-//                    System.out.println(r.getCurrentPositions().toString());
-//                });
-        //throw new RuntimeException("constraint violation");
+    public void generatePerfReport(PositionPerformanceRequest request) {
+        PositionPerformanceResponse response = positionPerfService.getPositionPerformance(request);
+        rabbitTemplate.convertAndSend("x.performance-report", "performance-report", response);
     }
 }
